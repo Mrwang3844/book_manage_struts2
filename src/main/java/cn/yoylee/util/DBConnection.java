ed749d16dee37491495a3123709421a573f99065
李洋洋 <19964803272@163.com>
2018-01-09 20:43:15 +0800

    修改数据连接池为c3p0,版本1.3

--- src/main/java/cn/yoylee/util/DBConnection.java
+++ src/main/java/cn/yoylee/util/DBConnection.java
@@ -1,23 +1,18 @@
 package cn.yoylee.util;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class DBConnection {
 
-    private static String driverName="com.mysql.cj.jdbc.Driver";
-    private static String userName="root";
-    private static String userPwd="lyy504677";
-    private static String dbName="book_db";
+    private static DataSource dataSource = new ComboPooledDataSource();
 
     public static Connection getDBConnection(){
-        String url1="jdbc:mysql://111.231.113.148:3306/"+dbName;
-        String url2="?user="+userName+"&password="+userPwd;
-        String url3="&useUnicode=true&characterEncoding=utf-8";
-        String url=url1+url2+url3;
         Connection con=null;
         try {
-            Class.forName(driverName);
-            con=DriverManager.getConnection(url);
+            con=dataSource.getConnection();
         } catch (Exception e) {
             e.printStackTrace();
         }
